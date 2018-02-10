734d1e0fad1f1767039acb65d01bd60a889377d3
Ben Lipson <sonofblip@gmail.com>
2018-02-10 10:07:16 -0600

    Clean up

--- main.hs
+++ main.hs
@@ -34,92 +34,43 @@ toDouble(Number n) = fromIntegral n
 escapedChars :: Parser String
 escapedChars = do
 	char '\\'
-	c <- oneOf "\\\"ntr"
-	case c of
-		'\\' -> do return [c]
-		'"' -> do return [c]
+	x <- oneOf "\\\"ntr"
+	case x of
+		'\\' -> do return [x]
+		'"' -> do return [x]
 		't' -> do return "\t"
 		'n' -> do return "\n"
 		'r' -> do return "\r"
-
-parseRatio :: Parser LispVal
-parseRatio = do
-	x <- many1 digit
-	char '/'
-	y <- many1 digit
-	return $ Ratio ((read x) % (read y))
-
-parseChar :: Parser LispVal
-parseChar = do
-	try $ string "#\\"
-	c <- many1 letter
-	return $ case (map toLower c) of
-		"space" -> Char ' '
-		"newline" -> Char '\n'
-		[x] -> Char x
-
-parseString :: Parser LispVal
-parseString = do
-	char '"'
-	x <- many $ many1 (noneOf "\"\\") <|> escapedChars
-	char '"'
-	return $ String (concat x)
 
 parseAtom :: Parser LispVal
 parseAtom = do
 	first <- letter <|> symbol
 	rest <- many (letter <|> digit <|> symbol)
 	let atom = first:rest
-	return $ Atom atom	
-
-parseBool :: Parser LispVal
-parseBool = do
-	string "#"
-	c <- oneOf "tf"
-	return $ case c of
-		't' -> Bool True
-		'f' -> Bool False
-
-parseDigital1 :: Parser LispVal
-parseDigital1 = do
-	c <- many1 digit
-	(return . Number . read) c
-
-parseDigital2 :: Parser LispVal
-parseDigital2 = do
-	try $ string "#d"
-	c <- many1 digit
-	(return . Number . read) c
-
-parseHex :: Parser LispVal
-parseHex = do
-	try $ string "#x"
-	c <- many1 hexDigit
-	return $ Number (hex2dig c)
-
-parseOct :: Parser LispVal
-parseOct = do
-	try $ string "#o"
-	c <- many1 octDigit
-	return $ Number (oct2dig c)
+	return $ Atom atom
 
 parseBin :: Parser LispVal
 parseBin = do
 	try $ string "#b"
-	c <- many1 (oneOf "10")
-	return $ Number (bin2dig c)
+	x <- many1 (oneOf "10")
+	return $ Number (bin2dig x)
 
-parseFloat :: Parser LispVal
-parseFloat = do
-	whole <- many1 digit
-	char '.'
-	decimal <- many1 digit
-	return $ Float (read (whole++"."++decimal) :: Double)
+parseBool :: Parser LispVal
+parseBool = do
+	string "#"
+	x <- oneOf "tf"
+	return $ case x of
+		't' -> Bool True
+		'f' -> Bool False
 
-parseNumber :: Parser LispVal
-parseNumber = do
-	num <- parseDigital1 <|> parseDigital2 <|> parseHex <|> parseOct <|> parseBin
-	return $ num
+parseChar :: Parser LispVal
+parseChar = do
+	try $ string "#\\"
+	x <- many1 letter
+	return $ case (map toLower x) of
+		"space" -> Char ' '
+		"newline" -> Char '\n'
+		[x] -> Char x
 
 parseComplex :: Parser LispVal
 parseComplex = do
@@ -129,10 +80,97 @@ parseComplex = do
 	char 'i'
 	return $ Complex (toDouble x :+ toDouble y)
 
+parseDigital1 :: Parser LispVal
+parseDigital1 = do
+	x <- many1 digit
+	(return . Number . read) x
+
+parseDigital2 :: Parser LispVal
+parseDigital2 = do
+	try $ string "#d"
+	x <- many1 digit
+	(return . Number . read) x
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+	head <- endBy parseExpr spaces
+	tail <- char '.' >> spaces >> parseExpr
+	return $ DottedList head tail
+
+parseFloat :: Parser LispVal
+parseFloat = do
+	whole <- many1 digit
+	char '.'
+	decimal <- many1 digit
+	return $ Float (read (whole++"."++decimal) :: Double)
+
+parseHex :: Parser LispVal
+parseHex = do
+	try $ string "#x"
+	x <- many1 hexDigit
+	return $ Number (hex2dig x)
+
+parseList :: Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+parseNumber :: Parser LispVal
+parseNumber = do
+	x <- parseDigital1 <|> parseDigital2 <|> parseHex <|> parseOct <|> parseBin
+	return $ x
+
+parseOct :: Parser LispVal
+parseOct = do
+	try $ string "#o"
+	x <- many1 octDigit
+	return $ Number (oct2dig x)
+
+parseQuasiQuoted :: Parser LispVal
+parseQuasiQuoted = do
+	char '`'
+	x <- parseExpr
+	return $ List [Atom "quasiquote", x]
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+	char '\''
+	x <- parseExpr
+	return $ List [Atom "quote", x]
+
+parseRatio :: Parser LispVal
+parseRatio = do
+	x <- many1 digit
+	char '/'
+	y <- many1 digit
+	return $ Ratio ((read x) % (read y))
+
+parseString :: Parser LispVal
+parseString = do
+	char '"'
+	x <- many $ many1 (noneOf "\"\\") <|> escapedChars
+	char '"'
+	return $ String (concat x)
+
+parseUnQuote :: Parser LispVal
+parseUnQuote = do
+	char ','
+	x <- parseExpr
+	return $ List [Atom "unquote", x]
+
 parseVector :: Parser LispVal
 parseVector = do
 	arrayValues <- sepBy parseExpr spaces
 	return $ Vector (listArray (0,(length arrayValues - 1)) arrayValues)
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+symbol :: Parser Char
+symbol = oneOf "!$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+	Left err -> "No match: " ++ show err
+	Right val -> "Found value"
 
 parseExpr :: Parser LispVal
 parseExpr = parseAtom
@@ -156,44 +194,6 @@ parseExpr = parseAtom
 		x <- try parseList <|> parseDottedList
 		char ')'
 		return x
-
-parseQuasiQuoted :: Parser LispVal
-parseQuasiQuoted = do
-	char '`'
-	x <- parseExpr
-	return $ List [Atom "quasiquote", x]
-
-parseUnQuote :: Parser LispVal
-parseUnQuote = do
-	char ','
-	x <- parseExpr
-	return $ List [Atom "unquote", x]
-
-parseList :: Parser LispVal
-parseList = liftM List $ sepBy parseExpr spaces
-
-parseDottedList :: Parser LispVal
-parseDottedList = do
-	head <- endBy parseExpr spaces
-	tail <- char '.' >> spaces >> parseExpr
-	return $ DottedList head tail
-
-parseQuoted :: Parser LispVal
-parseQuoted = do
-	char '\''
-	x <- parseExpr
-	return $ List [Atom "quote", x]
-
-spaces :: Parser ()
-spaces = skipMany1 space
-
-symbol :: Parser Char
-symbol = oneOf "!$%&|*+-/:<=>?@^_~"
-
-readExpr :: String -> String
-readExpr input = case parse parseExpr "lisp" input of
-	Left err -> "No match: " ++ show err
-	Right val -> "Found value"
 
 main :: IO ()
 main = do
